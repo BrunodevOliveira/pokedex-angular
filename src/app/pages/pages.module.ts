5ea60a6cb589c9d1c8095d82740eb578460ea400
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-04-28 22:48:05 -0300

    Criando e adicionando componentes compartilhados em home

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,9 @@
-import { RoutingModule } from './routing-module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing-module';
 
 
 
@@ -13,7 +14,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
